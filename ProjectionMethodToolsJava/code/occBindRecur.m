6767a3ee2002ff271743601338e6616814133007
Gary Anderson <asymptoticallystable@gmail.com>
2015-05-24 15:13:36 -0400

    recursive code works for simple model paths seem right for perfect foresight

--- ProjectionMethodToolsJava/code/occBindRecur.m
+++ ProjectionMethodToolsJava/code/occBindRecur.m
@@ -1,10 +1,11 @@
 PrependTo[$Path,"../../../paperProduction/mathAMA/AMAModel/"];
 PrependTo[$Path,"../../../mathAMA/NumericAMA"];
 PrependTo[$Path,"../../../mathAMA/SymbolicAMA"];
+Print["reading occBindRecur.m"]
 BeginPackage["occBindRecur`",{"ProtectedSymbols`","ProjectionInterface`","JLink`","AMAModel`","NumericAMA`"}]
 genCompSlackSysFunc::usage="genCompSlack[pathLen_Integer,zFuncs:{_Function...}]"
 fpForInitStateFunc::usage="fpForInitVecFunc[qVal,ruVal,epsVal,theCompSlackSysFunc_,zFuncs:{_Function...}]"
-
+makeInterpFuncPF::usage="makeInterpFunc";
 numericLinearizeSystemForOBC::usage="numericLinearizeSystemForOBC[eqns_List]"
 nonFPart::usage="nonFPart[xtm1_?MatrixQ,epsilon_?MatrixQ,bmat_?MatrixQ,phimat_?MatrixQ,psimat_?MatrixQ]"
 redoFPart::usage="redoFPart[phimat_?MatrixQ,fmat_?MatrixQ,psiz_?MatrixQ,horizon_Integer,numCon_Integer]"
@@ -37,7 +38,7 @@ psiz::usage="simple model matrix"
 psieps::usage="simple model matrix"
 
 
-numIt::usage="numIt[xx_]  does lucaSubs and leaves t values untouched by N"
+numIt::usage="numIt[xx_]  does lucaSubs and leaves t values untouched by N[]"
 
 
 
@@ -57,48 +58,22 @@ numIt[xx_]:=xx//.lucaSubs//myN//Expand//Chop
  
 
 
-genCompSlackSysFunc[pathLen_Integer,zFuncs:{_Function...}]:=
+genCompSlackSysFunc[pathLen_Integer]:=
 With[{aPath=genPath[pathLen],
 theZs=Flatten[genZVars[pathLen-1,1]]},
-With[{zFuncsApps=Drop[zFuncs[[-1]][qTry,rTry,0],2],
-compCon=((aPath[[5,1]]>=0.02&&theZs[[-1]]==0)||
-(aPath[[5,1]]==0.02&&theZs[[-1]]>=0))&&
-qTry==aPath[[4,1]]&&
-rTry==aPath[[6,1]]},
+With[{compCon=((aPath[[5,1]]>=0.02&&theZs[[-1]]==0)||
+(aPath[[5,1]]==0.02&&theZs[[-1]]>=0)),
+rhsEqns={aPath[[4,1]],aPath[[6,1]]}},
 With[{zLeft=(Drop[theZs,-1])},
-With[{zEqns=And @@ (Thread[zLeft==zFuncsApps])},
 With[{theSys=And[compCon,zEqns]},
-{zLeft,zEqns,zFuncsApps,compCon,theSys,
-Function @@ {{qTry,rTry},
-theSys}}]]]]]/;
-And[
-pathLen>0,
-Length[zFuncs]===pathLen-1,
-NumberQ[Plus @@ Flatten[Through[zFuncs[0,0,0]]]]]
+{compCon,rhsEqns}]]]]/;
+And[pathLen>0]
 
 
+noCnstrnGuess= Function @@ 
+{{qVal,ruVal},
+Flatten[bmat . {{qVal},{0},{ruVal}}+psieps.{{0}}][[{1,3}]]}
 
-
-fpForInitStateFunc[qVal_?NumberQ,ruVal_?NumberQ,epsVal_?NumberQ,
-theCompSlackSysFunc_,zFuncs:{_Function...}]:=With[{
-initGuess=If[True(*Length[zFuncs]==0*),
-Flatten[bmat . {{qVal},{0},{ruVal}}+psieps.{{epsVal}}][[{1,3}]],
-zFuncs[[-1]][qVal,ruVal,epsVal][[{1,2}]]],
-rhsEqns=List @@ theCompSlackSysFunc[[2,{2,3},2]]/.{qtm1->qVal,rutm1->ruVal,eps->epsVal},
-aPath=genPath[pathLen],
-theZs=Flatten[genZVars[pathLen-1,1]]},
-With[{zFuncsApps=Drop[zFuncs[[-1]][qTry,rTry,0],2],
-initStateSubbed=
-(theCompSlackSysFunc/.{qtm1->qVal,rutm1->ruVal,eps->epsVal})},
-With[{zLeft=(Drop[theZs,-1])},
-With[{zEqns=And @@ (Thread[zLeft==zFuncsApps])},
-With[{theSys=And[compCon,zEqns]},
-With[{zVars=Union[Cases[initStateSubbed,xx_[t],Infinity]]},
-{zVars,rhsEqns,initGuess,initStateSubbed,
-FixedPoint[Join[rhsEqns,zVars]/.Flatten[
-NSolve[initStateSubbed @@ #,zVars]]&,initGuess]}]]]]]]
-
-(*
 test01=Function[{qtm1, rutm1, eps}, 
  Piecewise[{{0, eps + 0.433733789322437*qtm1 + 0.5000000000000001*rutm1 > 
      0.03239935157289748}, {0.0430556167081694 - 1.3289036544850497*eps - 
@@ -108,12 +83,151 @@ test01=Function[{qtm1, rutm1, eps},
 
 
 z01ExactFunc=Function @@ With[{eval=test01[qtm1,rutm1,eps]},{{qtm1,rutm1,eps},
-Append[Flatten[(genPath[1][[{4,6}]])],Global`zzz$0$1[t]]/.
-Global`zzz$0$1[t]->eval}]
+Append[Flatten[(genPath[1][[{4,6}]])],zzz$0$1[t]]/.
+zzz$0$1[t]->eval}]
 
-{ig,ig,ig,ig,ig,ha2}=genCompSlackSysFunc[2,{z01ExactFunc}]
+fixZ01={
+Function[{xx,yy},z01ExactFunc[xx,yy,0][[1]]],
+Function[{xx,yy},z01ExactFunc[xx,yy,0][[2]]],
+Function[{xx,yy},z01ExactFunc[xx,yy,0][[3]]]}
 
-{zVars,rhsEqns,initGuess,initStateSubbed,fpnow}=fpForInitStateFunc[-.4,.1,0,ha2,{z01ExactFunc}]
+
+
+fpForInitStateFunc[qVal_?NumberQ,ruVal_?NumberQ,epsVal_?NumberQ,
+zFuncs_:fixZ01]:=
+With[{pathLen=Length[zFuncs]-1,
+valSubs={qtm1->qVal,rutm1->ruVal,eps->epsVal}},
+With[{csrhs=genCompSlackSysFunc[pathLen]/.valSubs,
+initGuess=If[Length[zFuncs]==0,
+noCnstrnGuess[qVal,ruVal][[{1,2}]],
+{zFuncs[[1]][qVal,ruVal],zFuncs[[2]][qVal,ruVal]}],
+aPath=genPath[pathLen],
+theZs=Flatten[genZVars[pathLen-1,1]]},
+With[{initStateSubbed=csrhs[[1]],
+tryEqnsSubbed=And @@Thread[{qTry,rTry}==(csrhs[[2]])]},
+With[{zLeft=(Drop[theZs,-1])},
+With[{theSys=Function[{qTry,rTry},
+With[{zFuncsApps=Through[Drop[zFuncs,2][qTry,rTry]]},
+With[{zEqns=And @@ (Thread[zLeft==zFuncsApps])},
+And[initStateSubbed,zEqns,tryEqnsSubbed]]]]},
+With[{zVars=Union[Cases[initStateSubbed,xx_[t],Infinity]]},
+With[{fpTarget=Join[{qTry,rTry},theZs]},
+If[Drop[Union[fpTarget],2]=!=Union[theZs],Print["diff zs",Drop[Union[fpTarget],2],Union[theZs],zEqns]];
+FixedPoint[fpTarget/.With[{soln=
+Flatten[NSolve[theSys @@ #,fpTarget]]},
+If[Not[MatchQ[soln,{(_->_)..}]],Throw[{"NSolve Failed in fpForInitState for",{theSys,fpTarget}}],soln]]&,initGuess]]]]]]]]
+
+
+
+
+makeInterpFuncPF[theFunc_Function,pos_List,iOrder_Integer,iPts_Integer,
+{qLow_?NumberQ,qHigh_?NumberQ},
+{ruLow_?NumberQ,ruHigh_?NumberQ}
+]:=Module[{thePts=
+gridPts[iPts,
+{qLow,qHigh},
+{ruLow,ruHigh}],
+pfFunc=Function[{qq,ru},theFunc[qq,ru,0]]},
+With[{whl={#,pfFunc @@ #}& /@
+thePts},
+Sow[theFunc];
+doScalarInterp[whl,#,iOrder]&/@pos]]/;
+With[{theRes=theFunc[0,0,0]},Print["iPts:theRes=",theRes];
+NumberQ[Plus @@ theRes[[pos]]]]
+
+makeInterpFuncPF[theFunc_Function,iOrder_Integer,iPts_Integer,
+{qLow_?NumberQ,qHigh_?NumberQ},
+{ruLow_?NumberQ,ruHigh_?NumberQ}
+]:=With[{pos=Range[Length[theFunc[0,0,0]]]},
+makeInterpFuncPF[theFunc,pos,iOrder,iPts,
+{qLow,qHigh},
+{ruLow,ruHigh}]]
+
+
+
+
+
+doScalarInterp[whlList:{{{_?NumberQ..},{_?NumberQ..}}..},pos_Integer,iOrder_Integer]:=
+With[{prtList={#[[1]],#[[2,pos]]}&/@whlList},
+Interpolation[prtList,InterpolationOrder->iOrder]]
+
+preCalcInterp[iOrder_Integer,iPts_Integer,
+{qLow_?NumberQ,qHigh_?NumberQ},
+{ruLow_?NumberQ,ruHigh_?NumberQ}]:=
+With[{theGrid=gridPts[iPts,
+{qLow,qHigh},
+{ruLow,ruHigh}],
+fns=Table[Unique["fnVal"],{(iPts+1)^3}]},
+With[{evals=Transpose[{theGrid,fns}]},
+{fns,evals,Interpolation[evals,InterpolationOrder->iOrder]}]]
+
+
+gridPts[iPts_Integer,
+qRng:{qLow_?NumberQ,qHigh_?NumberQ},
+rRng:{ruLow_?NumberQ,ruHigh_?NumberQ}]:=
+With[{
+qPts=oneDimGridPts[iPts,qRng],
+rPts=oneDimGridPts[iPts,rRng]},
+Flatten[Outer[List,qPts,rPts],1]]
+
+
+
+oneDimGridPts[iPts_Integer,{xLow_?NumberQ,xHigh_?NumberQ}]:=
+Table[ii,{ii,xLow,xHigh,N[xHigh-xLow]/iPts}]
+
+
+(*
+
+forIter[zFuncsNow_List]:=With[
+{fpSolnFunc=Function[{xx,yy,zz},fpForInitStateFunc[xx,yy,zz,zFuncsNow]]},
+makeInterpFuncPF[fpSolnFunc,1,numPts,{-.4,.4},{-.1,.1}]]
+
+
+
+
+{cc,rs}=genCompSlackSysFunc[1]
+
+{zVars,rhsEqns,initGuess,initStateSubbed,ts,fpnow}=
+fpForInitStateFunc[-.4,.1,0]
+
+{cc,rs}=genCompSlackSysFunc[2]
+
+{zVars,rhsEqns,initGuess,initStateSubbed,ts,fpnow}=
+fpForInitStateFunc[-.4,.1,0,z01ExactFunc]
+
+numPts=5
+
+
+Timing[doNow01= Function[{xx,yy,zz},fpForInitStateFunc[xx,yy,zz,fixZ01]]]
+Timing[nxt01=makeInterpFuncPF[doNow01,1,numPts,{-.4,.4},{-.1,.1}]]
+
+doNow02= Function[{xx,yy,zz},fpForInitStateFunc[xx,yy,zz,nxt01]]
+Timing[nxt02=makeInterpFuncPF[doNow02,1,numPts,{-.4,.4},{-.1,.1}]]                                     
+doNow03= Function[{xx,yy,zz},fpForInitStateFunc[xx,yy,zz,nxt02]]
+Timing[nxt03=makeInterpFuncPF[doNow03,1,numPts,{-.4,.4},{-.1,.1}]]                                     
+
+
+aPathFunc[qtm1Arg_?NumberQ,rutm1Arg_?NumberQ,epsArg_?NumberQ,zFuncs_List]:=  
+With[{pathLen=Length[zFuncs]-2},
+With[{tp=genPath[pathLen,1]/.{qtm1->qtm1Arg,rutm1->rutm1Arg,eps->epsArg},
+theZs=Flatten[genZVars[pathLen-1,1]]},
+With[{zLeft=(Drop[theZs,-0]),
+zFuncsApps=Through[Drop[zFuncs,2][qtm1Arg,rutm1Arg]]},
+With[{zAssn=Thread[zLeft->zFuncsApps]},
+tp/.zAssn
+]]]]
+
+try04=NestList[forIter,fixZ01,04];
+
+
+hmatApp[qtm1_?NumberQ,rutm1_?NumberQ,eps_?NumberQ,zFuncs_List]:=
+With[{tp=aPathFunc[qtm1,rutm1,eps,zFuncs]},
+With[{hApps=hmat.tp[[Range[9]+3*#]]&/@Range[0,(Length[tp]/3)-3]},
+Join@ hApps//Chop]]
+
+
+
+
 *)
 
 nonFPart[xtm1_?MatrixQ,epsilon_?MatrixQ,
@@ -205,5 +319,5 @@ Protect[hmat,qmat,bmat,phimat,fmat,psiz,psieps]
 
 End[]
 EndPackage[]
-
+Print["done reading occBindRecur.m"]
 
