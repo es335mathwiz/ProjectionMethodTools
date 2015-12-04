8bf1cb55e083c34829df626af6497b8d6b1c4afc
why <asymptoticallystable@gmail.com>
2015-12-04 09:21:54 -0500

    rm forlilmod.mth '

--- ProjectionMethodToolsJava/code/barthMarxRSwitch.m
+++ ProjectionMethodToolsJava/code/barthMarxRSwitch.m
@@ -9,7 +9,7 @@ BeginPackage["barthMarxRSwitch`",{"AMASeriesRepresentation`","ProtectedSymbols`"
 (*regime[t] like epsilon in timing, but "history matters"*)
 
 bmRSwitchEqnFuncs::usage="makeFunc[funcArgs,8,allFuncs]bmRSwitchEqnFuncs=makeFunc[funcArgs,8,allFuncs]"
-bmRSwitchProbFunc::usage="bmRSwitchProbFunc = Function @@{Append[funcArgs[[Range[4]]],epsVal]}"
+bmRSwitchProbFunc::usage="bmRSwitchProbFunc = Function @@{Append[funcArgs[[Range[3]]],epsVal]}"
 
 
 Begin["Private`"]
@@ -18,85 +18,72 @@ Begin["Private`"]
 
 
 
-
+  (*
 bmRSwitchEqns={
 $1ii[t] -($1thePi[t+1] + $1rr[t]),
 $1rr[t]= rho * $1rr[t-1] +eps[u][t],
-$1ii[t]= $1ap1ha * $1thePi[t],
+$1ii[t]= $1alpha * $1thePi[t],
 $2ii[t] -($2thePi[t+1] + $2rr[t]),
 $2rr[t]= rho * $2rr[t-1] +eps[u][t],
 $2ii[t]=$2alpha * $2thePi[t]
-}
+  }*)
 (*but see page 18*)
-bmRSwitchParamSubs={$1ap1ha->.9, 
-$2alpha->3,rho->.99,lam->.1,probGamma->1,
+bmRSwitchParamSubs={$0alpha->.9, 
+$1alpha->3,rho->.99,lam->.1,probGamma->1,
 $1gamma->.3,$2gamma->.2}
 
 
 
 funcArgs=
 {
-$1iitm1,
-$1rrtm1,
-$1thePitm1,
-$1regimetm1,
-$2iitm1,
-$2rrtm1,
-$2thePitm1,
-$2regimetm1,
-$1iit,
-$1rrt,
-$1thePit,
-$1regimet,
-$2iit,
-$2rrt,
-$2thePit,
-$2regimet,
-$1iitp1,
-$1rrtp1,
-$1thePitp1,
-$1regimetp1,
-$2iitp1,
-$2rrtp1,
-$2thePitp1,
-$2regimetp1,
-epsVal
+iitm1,
+rrtm1,
+thePitm1,
+iit,
+rrt,
+thePit,
+iitp1,
+rrtp1,
+thePitp1,
+epsVal,
+regimeVal
 }
 
 lilHDet0Guts={
-$1iit - ($1thePitp1 + $1rrt),
-$1rrt - ( rho * $1rrtm1 +epsVal),
-$1iit - $1ap1ha * $1thePit,
-$1regimet-1,
-$2iit - ($2thePitp1 + $2rrt),
-$2rrt - ( rho * $2rrtm1 +epsVal),
-$2iit - ($2alpha * $2thePit),
-$2regimet-2
+iit - (thePitp1 + rrt),
+rrt - ( rho * rrtm1 +epsVal),
+iit - $0alpha * thePit
+}
+lilHDet1Guts={
+iit - (thePitp1 + rrt),
+rrt - ( rho * rrtm1 +epsVal),
+iit - $1alpha * thePit
 }
 
 lilHDet0Func= Function @@ {funcArgs,lilHDet0Guts}//.bmRSwitchParamSubs;
+lilHDet1Func= Function @@ {funcArgs,lilHDet1Guts}//.bmRSwitchParamSubs;
 
 
-bigHDet0Guts={
-{0,0,-1,0,0,0,0,0},
-{0,0,0,0,0,0,0,0},
-{0,0,0,0,0,0,0,0},
-{0,0,0,0,0,0,0,0},
-{0,0,0,0,0,0,-1,0},
-{0,0,0,0,0,0,0,0},
-{0,0,0,0,0,0,0,0},
-{0,0,0,0,0,0,0,0}
+bigHDetGuts={
+{0,0,-1},
+{0,0,0},
+{0,0,0}
 }
 
 
-bigHDet0Func= Function @@ {funcArgs,bigHDet0Guts}//.bmRSwitchParamSubs;
+bigHDetFunc= Function @@ {funcArgs,bigHDetGuts}//.bmRSwitchParamSubs;
 
 
 (* {rho, $1ap1ha, $2alpha, the$1Pi[t], $1rr[-1 + t], $1thePi[1 + t], 
     $2rr[-1 + t], $2thePi[t], $2thePi[1 + t], eps[u][t]}*)
-allFuncs={lilHDet0Func,bigHDet0Func}
 
-bmRSwitchEqnFuncs=makeFunc[funcArgs,8,allFuncs]
+
+theSelFunc= Function @@{funcArgs,
+Switch[regimeVal,0,0,1,1]}
+allFuncs=
+  {theSelFunc,{{lilHDet0Func,bigHDetFunc},{lilHDet1Func,bigHDetFunc}}}
+
+bmRSwitchEqnFuncs=makeFunc[funcArgs,3,allFuncs]
 
 pBarMat={
 {.95, 0.05},
@@ -106,7 +93,8 @@ lambdaMat={
 {-lam,lam}}
 probGuts =pBarMat+probGamma*lambdaMat*($1thePitm1^2)
 
-bmRSwitchProbFunc = Function @@{Append[funcArgs[[Range[4]]],epsVal],probGuts}//.bmRSwitchParamSubs;
+bmRSwitchProbFunc = Function @@
+{Append[funcArgs[[Range[3]]],epsVal],probGuts}//.bmRSwitchParamSubs;
 
 (*
 
@@ -116,4 +104,5 @@ pp[ii_Integer,jj_Integer]:= ppBar[ii,jj] + gamma lambda[ii,jj]*$1thePi[t-1]^2
 
 End[]
 EndPackage[]
+Print["done reading barthMarxRSwitch.m"]
 
